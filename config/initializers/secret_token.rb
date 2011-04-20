ec96ab6c88316047f52a76306ed53d7b59b26e1f
Thomas Buchöster <tbuchoester.mmt-b2009@fh-salzburg.ac.at>
2011-04-20 15:56:04 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nineminutes::Application.config.secret_token = 'ce6034e5aa206913f2bfdc940c4f158120f9a174147463d32e02528337074676adff1118de7d11b6040602de0d8bdba53a59720fcc9c2e3ecda1409983b0b3fd'
