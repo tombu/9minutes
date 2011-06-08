93bf60fc57b81e6b81d0ed9761a3e37d6872627a
Thomas Buchöster <tbuchoester.mmt-b2009@fh-salzburg.ac.at>
2011-06-08 03:58:39 +0200

    corrected two mistakes caused by class name change in LastFM lib

--- lib/last_fm/chart.rb
+++ lib/last_fm/chart.rb
@@ -4,14 +4,14 @@ module LastFM
     def self.getTopArtists limit = nil, page = nil
       chart_request "getTopArtists", "artists", limit, page do |artists|
         LastFM::Validator.validate_mash artists, "image"
-        update_results "#{current_class_name}_#{__method__}", artists
+        update_results [current_class_name, __method__], artists
       end
     end
 
     def self.getTopTracks limit = nil, page = nil
       chart_request "getTopTracks", "tracks", limit, page do |tracks|
         LastFM::Validator.validate_mash tracks
-        update_results "#{current_class_name}_#{__method__}", tracks
+        update_results [current_class_name, __method__], tracks
       end
     end
   end
