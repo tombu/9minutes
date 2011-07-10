a532b14da805007a9aa02c2fd8b2c544cf4498ea
Thomas Buchöster <tbuchoester.mmt-b2009@fh-salzburg.ac.at>
2011-07-10 11:01:58 +0200

    added 'lib' directory to autoload_once_paths in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Nineminutes::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.autoload_once_paths += %W(#{config.root}/lib)
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
