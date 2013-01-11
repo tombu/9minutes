8ac287e8bd6a2870b556b4b0051ad5a5533529ca
Thomas Buchöster <thomas.buchoester@gmail.com>
2013-01-11 23:44:01 +0100

    Adds image formats to precompile list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Nineminutes::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
